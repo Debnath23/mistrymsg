1754233ea4ba3eec71965db4b27e8091957e3d7c
Debnath Mahapatra <128223356+Debnath23@users.noreply.github.com>
2024-04-23 12:30:58 +0530

    User schema in mongodb & zod schemas

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const usernameValidation = z
+  .string()
+  .min(2, "username must be atleast 2 characters")
+  .max(20, "username must be not more than 20 characters")
+  .regex(/^[a-zA-Z0-9_]+$/, "username must not contain any special character");
+
+export const signUpSchema = z.object({
+  username: usernameValidation,
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" }),
+});
